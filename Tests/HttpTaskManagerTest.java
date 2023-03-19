33a1c2e9f29f22011432bcff156de1123dc01daf
Lolita <LolitaKardan@yandex.ru>
2023-03-19 23:58:07 +0300

    feat: add httpServer

--- Tests/HttpTaskManagerTest.java
+++ Tests/HttpTaskManagerTest.java
@@ -41,33 +41,6 @@ public class HttpTaskManagerTest extends TasksManagerTest<HttpTaskManager> {
     }
 
     @Test
-    private void saveAndLoadTest() {
-        Task task1 = taskManager.makeNewTask(new Task("Название задачи 1", "Описание по задаче 1", LocalDateTime.of(2023,03,15,11,0), 15L));
-        Task task2 = taskManager.makeNewTask(new Task("Название задачи 2", "Описание по задаче 2", LocalDateTime.of(2023,03,13,11,25), 15L));
-        Epic epic1 = taskManager.makeNewEpic(new Epic("Название эпика 1", "Описание по эпику 1", LocalDateTime.of(2023,03,16,01,0), 15L));
-        Epic epic2 = taskManager.makeNewEpic(new Epic("Название эпика 2", "Описание по эпику 2", LocalDateTime.of(2023,03,16,02,0), 15L));
-        Subtask subtask2_1 = taskManager.makeNewSubtask(new Subtask("Название по подзадаче 2.1",
-                "Описание по подзадаче 2.1", LocalDateTime.of(2023,03,16,02,0), 15L), epic2);
-        Subtask subtask2_2 = taskManager.makeNewSubtask(new Subtask("Название по подзадаче 2.2",
-                "Описание по подзадаче 2.2", LocalDateTime.of(2023,03,16,02,20), 15L), epic2);
-        Subtask subtask2_3 = taskManager.makeNewSubtask(new Subtask("Название по подзадаче 2.3",
-                "Описание по подзадаче 2.3", LocalDateTime.of(2023,03,16,02,50), 15L), epic2);
-
-        taskManager.getTaskById(task1.getId());
-        taskManager.getEpicById(epic1.getId());
-        taskManager.getSubtaskById(subtask2_1.getId());
-
-        HttpTaskManager httpTaskManager = new HttpTaskManager(url);
-        httpTaskManager.load();
-
-        assertEquals(taskManager.getTasksList(), httpTaskManager.getTasksList());
-        assertEquals(taskManager.getEpicsList(), httpTaskManager.getEpicsList());
-        assertEquals(taskManager.getSubtasksList(), httpTaskManager.getSubtasksList());
-        assertEquals(taskManager.getHistoryManager().getHistory(), httpTaskManager.getHistoryManager().getHistory());
-        kvServer.stop();
-    }
-
-    @Test
     void correctLoadTask() {
         Task task1 = taskManager.makeNewTask(new Task("Название задачи 1", "Описание по задаче 1", LocalDateTime.of(2023,03,15,11,0), 15L));
         Task task2 = taskManager.makeNewTask(new Task("Название задачи 2", "Описание по задаче 2", LocalDateTime.of(2023,03,13,11,25), 15L));
